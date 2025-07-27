4119b5adde4ac2dbccb2ac6e688d299023c732d9
CoolGitGuy <borisboskovic16@icloud.com>
2025-07-27 19:59:00 +0200

    Updated MVVM

---
+++ MessageApp/MVVM/ViewModelBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageApp.MVVM
+{
+    public partial class ViewModelBase : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string property = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
