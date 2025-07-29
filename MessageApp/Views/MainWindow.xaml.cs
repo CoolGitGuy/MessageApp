c9b6d75e9c4bb8e32cdfd5397b75621e2f7c8e32
CoolGitGuy <borisboskovic16@icloud.com>
2025-07-29 03:09:20 +0200

    Added Minor Changes And Verification Code

--- MessageApp/Views/MainWindow.xaml.cs
+++ MessageApp/Views/MainWindow.xaml.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using MessageApp.ViewModels;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -11,10 +7,15 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Graphics;
-using Microsoft.UI.Windowing;
+using Windows.UI.WindowManagement;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -30,11 +31,21 @@ namespace MessageApp
 
         public MainWindow()
         {
-            this.AppWindow.Resize(new SizeInt32(600, 400));
-            ExtendsContentIntoTitleBar = true;
+            WindowSettings();
 
             InitializeComponent();
             this.StackPanel.DataContext = ViewModel;
+        }
+
+        public void WindowSettings() //Disables Resize And Removes Title Bar
+        {
+            if (this.AppWindow.Presenter is OverlappedPresenter presenter)
+            {
+                presenter.IsResizable = false;
+                //presenter.SetBorderAndTitleBar(false, false);
+            }
+            this.AppWindow.Resize(new SizeInt32(600, 400));
+            ExtendsContentIntoTitleBar = true;
         }
     }
 }
