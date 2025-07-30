74bf2427259f9880e421704d46371938b3cbcb8e
CoolGitGuy <borisboskovic16@icloud.com>
2025-07-30 03:01:44 +0200

    Added Navigation Logic

---
+++ MessageApp/Services/WindowService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageApp.Services
+{
+    public class WindowService : IWindowService
+    {
+        private Window currentWindow;
+
+        public WindowService(Window currentWindow)
+        {
+            this.currentWindow = currentWindow;
+        }
+
+        public void closeCurrentWindow()
+        {
+            currentWindow.Close();
+        }
+
+        public void openWindow<T>() where T : Window, new()
+        {
+            var newWindow = new T();
+            newWindow.Activate();
+        }
+    }
+}
---
+++ MessageApp/ViewModels/VerificationWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using MessageApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageApp.ViewModels
+{
+    public partial class VerificationWindowViewModel
+    {
+        public VerificationWindowViewModel(IWindowService windowService)
+        {
+            
+        }
+    }
+}
